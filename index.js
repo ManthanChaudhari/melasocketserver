ec1fa1c93cbd7e5fb3ee1788a4436ecc046733c4
ManthanChaudhari <121889086+Manthanchau@users.noreply.github.com>
2024-04-18 18:21:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import { Server } from "socket.io";
+import {createServer} from "http"
+import cors from "cors"
+
+const port = 3001;
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+    cors : {
+        origin : "https://chat-web-app-alpha.vercel.app",
+        methods : ["GET" , "POST"],
+        credentials : true
+    }
+})
+
+app.get("/" , (req,res) => {
+    res.send("Hello World!");
+});
+
+app.use(cors({
+    origin : "https://chat-web-app-alpha.vercel.app",
+    methods : ["GET" , "POST"],
+    credentials : true
+}));
+io.on("connection" , (socket) => {
+    console.log("User Connected");
+    console.log("id" , socket.id);
+   
+    socket.on("message" , ({userData , user , singleMessage}) => {
+        console.log(`${userData.name}  :  ${singleMessage}`);
+        io.to(user).emit("receive-msg" , {userData , singleMessage });
+    })
+    socket.on("join-user" , ({userData , roomCode}) => {
+        socket.join(roomCode);
+        io.to(roomCode).emit("user-joined" , userData);
+    })
+    socket.on("disconnect" , () => {
+        console.log("User Disconnected" , socket.id);
+        socket.broadcast.emit("disconnected" , socket.id);
+    })
+})
+server.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+});
